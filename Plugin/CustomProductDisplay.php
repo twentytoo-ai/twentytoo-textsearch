fd83345fcc6eb88e9659c8b41523dff353b19091
test <test@9750-5007-6974>
2024-05-20 15:41:55 +0300

    update

--- Plugin/CustomProductDisplay.php
+++ Plugin/CustomProductDisplay.php
@@ -1,61 +1,51 @@
 <?php
 namespace TwentyToo\TextSearch\Plugin;
 
-use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\FilterBuilder;
 use Psr\Log\LoggerInterface;
-use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection;
 
-class CustomProductDisplay
+class CustomProductSearch
 {
-    protected $productRepository;
+    protected $searchCriteriaBuilder;
+    protected $filterBuilder;
     protected $logger;
 
     public function __construct(
-        ProductRepositoryInterface $productRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
         LoggerInterface $logger
     ) {
-        $this->productRepository = $productRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
         $this->logger = $logger;
     }
 
-    public function afterGetItems(Collection $subject, $result)
-    {
-        $this->logger->info('Custom products display plugin triggered');
+    public function beforeSearch(
+        \Magento\CatalogSearch\Model\Search\Search $subject,
+        $requestName,
+        \Magento\Framework\Api\Search\SearchCriteriaInterface $searchCriteria
+    ) {
+        $this->logger->info('Custom product search plugin triggered');
 
         // Static product IDs for testing purposes
         $productIds = [1, 2];
         $this->logger->info('Static product IDs ----> ' . json_encode($productIds));
 
-        $customProducts = [];
-        foreach ($productIds as $productId) {
-            try {
-                // Fetch the product object by its ID
-                $product = $this->productRepository->getById((int)$productId); // Ensure ID is an integer
-                if ($product) {
-                    // Add the product object to the customProducts array
-                    $customProducts[] = $product;
-                    // Log details for debugging
-                    $this->logger->info('Loaded product ID: ' . $product->getId());
-                    $this->logger->info('Product Name: ' . $product->getName());
-                    $this->logger->info('Product Visibility: ' . $product->getVisibility());
-                    $this->logger->info('Product Status: ' . $product->getStatus());
-                    $this->logger->info('Product Image: ' . $product->getImage());
-                } else {
-                    $this->logger->error('Product ID ' . $productId . ' not found.');
-                }
-            } catch (\Exception $e) {
-                $this->logger->error('Error loading product ID ' . $productId . ': ' . $e->getMessage());
-            }
-        }
+        // Create a filter for the custom product IDs
+        $filter = $this->filterBuilder
+            ->setField('entity_id')
+            ->setValue($productIds)
+            ->setConditionType('in')
+            ->create();
 
-        // Append custom products to the existing collection
-        foreach ($customProducts as $product) {
-            $subject->addItem($product);
-        }
+        // Create a new search criteria with the custom product IDs
+        $this->searchCriteriaBuilder->addFilters([$filter]);
+        $newSearchCriteria = $this->searchCriteriaBuilder->create();
 
-        $this->logger->info('Final combined result count: ' . count($subject->getItems()));
+        $this->logger->info('Modified search criteria: ' . json_encode($newSearchCriteria->getFilterGroups()));
 
-        // Return the modified collection
-        return $subject;
+        // Return the modified search criteria to replace the original
+        return [$requestName, $newSearchCriteria];
     }
 }
