9bd879b091b310b7a3ba784ef0fc7c61e58dae46
test <test@9750-5007-6974>
2024-05-23 09:32:09 +0300

    update

---
+++ Observer/SearchQueryObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace TwentyToo\TextSearch\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+use TwentyToo\TextSearch\Service\ApiService;
+use Magento\Framework\App\ResourceConnection;
+
+class SearchQueryObserver implements ObserverInterface
+{
+    protected $logger;
+    protected $apiService;
+    protected $resourceConnection;
+
+    public function __construct(
+        LoggerInterface $logger,
+        ApiService $apiService,
+        ResourceConnection $resourceConnection
+    ) {
+        $this->logger = $logger;
+        $this->apiService = $apiService;
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    public function execute(Observer $observer)
+    {
+        // Log that the observer is being executed
+        $this->logger->info('SearchQueryObserver: Observer executed.');
+
+        $query = $observer->getEvent()->getData('query')->getQueryText();
+        $this->logger->info('Search query: ' . $query);
+
+        $productIds = $this->apiService->getProductIdsFromApi($query);
+        $this->logger->info('Service Products ' . $productIds);
+
+        if (!empty($productIds)) {
+            $this->logger->info('Product IDs from API: ' . implode(', ', $productIds));
+            $this->replaceSearchResults($productIds);
+        }
+    }
+
+    protected function replaceSearchResults($productIds)
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $tableName = $connection->getTableName('catalogsearch_fulltext_scope1');
+
+        $query = "DELETE FROM $tableName WHERE product_id NOT IN (" . implode(',', $productIds) . ")";
+        $connection->query($query);
+
+        $this->logger->info('Search results replaced with product IDs.');
+    }
+}
