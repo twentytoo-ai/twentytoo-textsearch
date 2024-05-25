7151818eda4224e14223c2db1018e510dc5e4087
test <test@9750-5007-6974>
2024-05-25 15:16:45 +0300

    update

--- Block/CustomProductDisplay.php
+++ Block/CustomProductDisplay.php
@@ -4,45 +4,54 @@ namespace TwentyToo\TextSearch\Block;
 use Magento\Framework\View\Element\Template;
 use Magento\Catalog\Model\ProductFactory;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\Registry;
 
 class CustomProductDisplay extends Template
 {
     protected $productFactory;
     protected $logger;
+    protected $registry;
 
     public function __construct(
         Template\Context $context,
         ProductFactory $productFactory,
         LoggerInterface $logger,
+        Registry $registry,
         array $data = []
     ) {
         $this->productFactory = $productFactory;
         $this->logger = $logger;
+        $this->registry = $registry;
         parent::__construct($context, $data);
     }
 
     public function getProducts()
     {
-        // Static product IDs for testing purposes
-        $productIds = [1, 2, 1];
-        $this->logger->info('Custom products display ----> ' . json_encode($productIds));
+        // Retrieve product IDs from the registry
+        $productIds = $this->registry->registry('custom_data_key');
         
+        if (!$productIds) {
+            $this->logger->info('No product IDs found in the registry.');
+            // return [];
+        }
+
+        $this->logger->info('Custom products display Block ----> ' . json_encode($productIds));
+        $staticIds = [1,1,1,1,1,1,1,1,1,1,1];
         $products = [];
-        foreach ($productIds as $productId) {
+        foreach ($staticIds as $productId) {
             $product = $this->productFactory->create()->load($productId);
             if ($product->getId()) {
                 $this->logger->info('Loaded product ID: ' . $product->getId());
                 $this->logger->info('Product Name: ' . $product->getName());
                 $this->logger->info('Product Visibility: ' . $product->getVisibility());
                 $this->logger->info('Product Status: ' . $product->getStatus());
-                //$this->logger->info('Product Stock: ' . $product->getExtensionAttributes()->getStockItem()->getIsInStock());
                 $this->logger->info('Product Image: ' . $product->getImage());
                 array_push($products, $product); // Use array_push to add product to array
             } else {
                 $this->logger->info('Product ID ' . $productId . ' could not be loaded.');
             }
         }
-        
+
         return $products;
     }
 
--- Observer/SearchQueryObserver.php
+++ Observer/SearchQueryObserver.php
@@ -7,21 +7,25 @@ use Magento\Framework\Event\ObserverInterface;
 use Psr\Log\LoggerInterface;
 use TwentyToo\TextSearch\Service\ApiService;
 use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Registry;
 
 class SearchQueryObserver implements ObserverInterface
 {
     protected $logger;
     protected $apiService;
     protected $session;
+    protected $registry;
 
     public function __construct(
         LoggerInterface $logger,
         ApiService $apiService,
-        SessionManagerInterface $session
+        SessionManagerInterface $session,
+        Registry $registry
     ) {
         $this->logger = $logger;
         $this->apiService = $apiService;
         $this->session = $session;
+        $this->registry = $registry;
     }
 
     public function execute(Observer $observer)
@@ -33,12 +37,15 @@ class SearchQueryObserver implements ObserverInterface
         $this->logger->info('Search query: ' . $queryText);
 
         $productIds = $this->apiService->getProductIdsFromApi($queryText);
-        $this->logger->info('Service Products: ' . json_encode($productIds));
+        $this->logger->info('Service Products Observer: ' . json_encode($productIds));
 
         if (!empty($productIds)) {
             $this->session->setCustomProductIds($productIds);
             $this->session->setSearchQuery($queryText);
             $this->logger->info('Session product IDs and query set.');
+
+            // Register custom data in the registry
+            $this->registry->register('custom_data_key', $productIds);
         }
     }
 }
